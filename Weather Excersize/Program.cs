1df78ec1d04fcf980523664b1d4ce0361c5155a7
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-08-03 09:03:42 -0600

    Finished Weather excersize

--- Weather Excersize/Program.cs
+++ Weather Excersize/Program.cs
@@ -1,4 +1,16 @@
-﻿
+﻿using Newtonsoft.Json.Linq;
+
+Console.WriteLine("Enter your City name");
+string cityName = Console.ReadLine();
+Console.WriteLine();
 
 
-var weatherAppUrl = https://api.openweathermap.org/data/2.5/weather?zip={zipCode},{countryCode}&appid={}
+Console.WriteLine("Enter the API Key");
+string key = Console.ReadLine();
+Console.WriteLine();
+
+var client = new HttpClient();
+var weatherAppUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={key}";
+var appResponse = client.GetStringAsync(weatherAppUrl).Result;
+var weather = JObject.Parse(appResponse).GetValue("main");
+Console.WriteLine($"The temp where you are is:{weather["temp"]} Degrees");
